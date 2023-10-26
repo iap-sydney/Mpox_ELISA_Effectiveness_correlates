61ddf130c7a01ab7af079d4dd98ea5567d903ecd
mtb980 <51979570+mtb980@users.noreply.github.com>
2023-10-26 13:28:11 +1100

    separate figure and analysis of effectiveness and tidy code

--- Analysis/01_Efficacy-fit.R
+++ Analysis/01_Efficacy-fit.R
@@ -1,28 +1,34 @@
-df_data<-read_csv('Output/Data/Eff.csv') %>%
-  mutate(Study=gsub('\\d\\d-','',Study))%>%
-  replace(is.na(.),0)
+# -------------------------------------------------------------------------
+#'Code used to estimate the effectiveness of the different vaccines including
+#'comparison of one and two doses of MVA-BN
+#
+# -------------------------------------------------------------------------
 
+
+
+# Import Data -------------------------------------------------------------
+
+#Load the Effectiveness data
+df_data<-read_csv('Output/Data/Eff.csv') %>%
+  mutate(Indiv=if_else(Dose==0,'Unvaccinated',paste(Vaccine,Study,Dose)),
+         Vaccinated=if_else(Dose==0,'Unvaccinated','Vaccinated'),
+         index=Study)%>%
+  replace(is.na(.),0)%>%
+  mutate_at(c('Vaccinated','Indiv','index','group','Vacc_dose'),as.factor)
+
+#Load efficacy study data
 df_studies<-read_csv('Data/Efficacy_studies.csv')
 
-df_data=mutate(df_data,Indiv=if_else(Dose==0,'Unvaccinated',paste(Vaccine,Study,Dose)))
-df_data$Indiv<-gsub('Unvaccinated \\w*','Unvaccinated',df_data$Indiv)
-df_data$Vaccinated=df_data$Vaccine
-df_data$Vaccinated[df_data$Vaccine!='Unvaccinated']<-'Vaccinated'
-
-df_data$Vaccinated=as.factor(df_data$Vaccinated)
-df_data$Indiv=as.factor(df_data$Indiv)
-df_data$index=as.factor(df_data$Study)
-df_data$group=as.factor(df_data$group)
-df_data$Vacc_dose=as.factor(df_data$Vacc_dose)
-
+#Create Data Frame for 2 dose dataset
 df_2dose=filter(df_data,Dose==2)%>%
   mutate(Indiv=fct_drop(Indiv))
 
-df_unvacc=df_data[df_data$Vaccine=='Unvaccinated',]
-df_vacc=df_data[df_data$Vaccine!='Unvaccinated',]
 
+# Analyse combined estimates of effectiveness -----------------------------
+
+#Create Data list for Stan
 data <- list(
-    J = nrow(df_vacc),
+    J = nrow(df_data),
     Num_pop = df_data$Pop,
     num_cases=df_data$Case,
     num_groups=nlevels(df_data$group),
@@ -32,56 +38,53 @@ data <- list(
     num_vacc_studies=nlevels(df_data$index),
     vacc_study_ind=as.integer(df_data$index)
 )
-  
+ 
+#Run Stan algorithm for sampling 
 eff_fit <- stan(
-    file = "Stan/Efficacy_regress.stan",  # Stan program
+    file = "Stan/Effectiveness_combined.stan",  # Stan program
     data = data,    # named list of data
     chains = 4,             # number of Markov chains
-    warmup = 5000,          # number of warmup iterations per chain
-    iter = 35000,            # total number of iterations per chain
+    warmup = warmup,          # number of warmup iterations per chain
+    iter = sampling,            # total number of iterations per chain
     seed=10,
     cores=4,
     control = list(max_treedepth=12,stepsize=0.5,adapt_delta=0.9)
 )
-eff_fit_pooled <- stan(
-  file = "Stan/Efficacy_pooled.stan",  # Stan program
-  data = data,    # named list of data
-  chains = 4,             # number of Markov chains
-  warmup = 10000,          # number of warmup iterations per chain
-  iter = 40000,            # total number of iterations per chain
-  seed=10,
-  cores=4,
-  control = list(max_treedepth=12,stepsize=0.5,adapt_delta=0.9)
-)
-  
-samples<-rstan::extract(eff_fit)
-Effs<-(samples$p_s)
-quants<-t(apply(Effs,2,quantile,probs=c(0.025,0.5,0.975)))
-df_output<-data.frame(quants)
-df_output$Study=levels(df_data$index)
 
-plot1<-ggplot(df_output,aes(x=Study,y=X50.))
-plot1<-plot1+geom_point(position=position_dodge(width=0.5))
-plot1<-plot1+geom_errorbar(aes(ymin=X2.5.,ymax=X97.5.), position=position_dodge(width=0.5))
-show(plot1)
-
+#Extract samples from RStan Object
 samples<-rstan::extract(eff_fit)
+#Determine posterior samples of effectiveness and summary statistics
 Effs<-100*(1-exp(samples$V))
 quants<-t(apply(Effs,2,quantile,probs=c(0.025,0.5,0.975)))
+#Create Data Frame for combined estimates
 df_output_combined<-data.frame(quants)
 mean_quant=quantile(samples$E,probs=c(0.025,0.5,0.975))
 df_output_combined$Vaccine=c('First Generation','MVA-BN 1-dose','MVA-BN 2-dose')
 df_output_combined$Study='Combined'
+write_csv(df_output_combined,'Output/Figure1/combined_eff.csv')
 
-RE_comb<-quantile(100*(1-exp(samples$V[2,]-samples$V[3,])),probs=c(0.025,0.5,0.975))
+#Calculate posterior samples and summaries for RE_combined
+RE<-exp(samples$V[,3]-samples$V[,2])
+REquant=c(quantile(RE,probs=c(0.025,0.5,0.975)))
+Com_RE<-tibble(REquant)%>%
+  mutate(Vaccine='MVA-BN\n',
+         Study='Combined',
+         y=c('X1','X2','X3'))%>%
+  pivot_wider(names_from = 'y',values_from = REquant)%>%
+  write_csv('Output/Figure1/RE_comb.csv')
 
+#Save Posterior samples of effectiveness.
 Eff_df<-data.frame(Effs)
 colnames(Eff_df)<-c('First Generation','MVA-BN 1-dose','MVA-BN 2-dose')
-write_csv(Eff_df,"Output//Data//Efficacy.csv")
+Eff_df$`Risk Ratio`<-RE
+write_csv(Eff_df,"Output//Samples//Combined-Effectiveness-sample.csv")
 
 
+# Analyse Data for individual Effectiveness -------------------------------
+
+#Create Data list for Stan
 data2 <- list(
-  J = nrow(df_vacc),
+  J = nrow(df_data),
   Num_pop = df_data$Pop,
   num_cases=df_data$Case,
   num_groups=nlevels(df_data$group),
@@ -90,19 +93,21 @@ data2 <- list(
   Vaccine_ind=as.integer(df_data$Indiv)
 )
 
-
+#Run RStan Model
 eff_fit_individual <- stan(
-  file = "Stan/Efficacy.stan",  # Stan program
+  file = "Stan/Effectiveness_individual.stan",  # Stan program
   data = data2,    # named list of data
   chains = 4,             # number of Markov chains
-  warmup = 10000,          # number of warmup iterations per chain
-  iter = 40000,            # total number of iterations per chain
+  warmup = warmup,          # number of warmup iterations per chain
+  iter = sampling,            # total number of iterations per chain
   seed=10,
   cores=4,
   control = list(max_treedepth=12,stepsize=0.5,adapt_delta=0.9)
 )
 
+#Extract Samples
 samples2<-rstan::extract(eff_fit_individual)
+#Calculate per study effectiveness
 Effs<-100*(1-exp(samples2$V))
 quants<-t(apply(Effs,2,quantile,probs=c(0.025,0.5,0.975)))
 df_output_indiv<-data.frame(quants)
@@ -111,8 +116,9 @@ df_output_indiv$Study<-gsub('\\w*\\W','',df_output_indiv$Study)
 df_output_indiv$Vaccine<-c(gsub('MVA-BN1','MVA-BN 1-dose',gsub(' [A-Z,a-z]*','',levels(df_data$Indiv)[1:15])))
 df_output_indiv$Vaccine<-gsub('Dryvax1','First Generation',df_output_indiv$Vaccine)
 df_output_indiv$Vaccine<-as.numeric(as.factor(gsub('MVA-BN2','MVA-BN 2-dose',df_output_indiv$Vaccine)))
-df_output=rbind(df_output_indiv,df_output_combined)
+write_csv(df_output_indiv,'Output/Figure1/Indiv_eff.csv')
 
+#Calculate per study Risk Ratio for 1 to 2 doses MVA-BN
 dose2_studies<-levels(df_2dose$Indiv)
 studies<-levels(df_data$Indiv)
 N2<-length(dose2_studies)
@@ -125,142 +131,13 @@ for (i in 1:N2){
   RE<-exp(samples2$V[,j]-samples2$V[,k])
   REtab[i,]=quantile(RE,probs=c(0.025,0.5,0.975))
 }
-RE<-exp(samples$V[,3]-samples$V[,2])
-#REtab[N2+1,]
-REquant=c(quantile(RE,probs=c(0.025,0.5,0.975)))
 REtib<-tibble(REtab)%>%
-  mutate(Vaccine='MVA-BN\n',Study=gsub(' 2','',c(gsub('MVA-BN ','',dose2_studies))))
-Com_RE<-tibble(REquant)%>%
-  mutate(Vaccine='MVA-BN\n',Study='Combined')
-
-#col_scale<- qualitative_hcl(11)
-
-Vacc_labs<-c('First Generation','MVA-BN 1-dose','MVA-BN 2-doses')
-Vacc_labs<-c('1st Gen','MVA-BN\n1-Dose','MVA-BN\n2-Dose')
-eff_plot<-ggplot(df_output_indiv,aes(x=Vaccine,y=X50.,ymin=X2.5.,ymax=X97.5.,shape=Study,group=Study,color=Study))+
-  geom_point(position=position_dodge(width=0.4),size=2,alpha=1)+
-  geom_errorbar( position=position_dodge(width=0.4),alpha=1)+
-  geom_line(position=position_dodge2(width=0.4),alpha=0.7)+
-  geom_point(data=df_output_combined,shape=16,size=2)+
-  geom_errorbar(data=df_output_combined)+
-  scale_y_continuous(limits=c(0,100))+
-  labs(y='Vaccine Effectiveness (%)') +
-  scale_x_discrete(labels=Vacc_labs) +
-  theme_classic() +
-  theme(axis.title.x=element_blank(),
-        axis.text = element_text(size=10),
-        axis.title = element_text(size=12),
-        legend.text = element_text(size=10))+
-  scale_shape_manual(name='Study',
-                     breaks=c(df_studies$Author,'Combined'),
-                     values=c(1:nlevels(as.factor(df_output_indiv$Study)),16),
-                     labels=c(df_studies$Label,'Combined Estimate'))+
-  scale_color_manual(name='Study',
-                     breaks=c(df_studies$Author,'Combined'),
-                     values=c(col_scale,'black'),
-                     labels=c(df_studies$Label,'Combined Estimate'),
-                     guide=guide_legend(override.aes = list(shape=c(1:nlevels(as.factor(df_output_indiv$Study)),16))))
-show(eff_plot)
-
-ggsave('Output//Figures//Efficacy_comparison.png',plot=eff_plot,height=5,width = 8,dpi=600)
-ggsave('Output//Figures//Efficacy_comparison.pdf',plot=eff_plot,height=5,width = 8,dpi=600)
+  mutate(Vaccine='MVA-BN\n',
+         Study=gsub(' 2','',c(gsub('MVA-BN ','',dose2_studies))))%>%
+  write_csv('Output/Figure1/REtib.csv')
 
 
-plot_RE<-ggplot(REtib,aes(x=Vaccine,y=X2,ymin=X1,ymax=X3,shape=Study,color=Study))+
-  geom_point(position=position_dodge(width=0.5),size=2,alpha=1)+
-  geom_errorbar(position=position_dodge(width=0.5),alpha=1)+
-  geom_point(aes(x='MVA-BN\n',y=REquant[2],shape='Combined',color='Combined'),size=2)+
-  geom_errorbar(aes(x='MVA-BN\n',ymin=REquant[1],ymax=REquant[3],color='Combined'))+
-  scale_y_continuous(limits=c(0,1.5))+
-  labs(y='Risk Ratio\n(MVA-BN 2-Dose vs 1-Dose)',x='')+
-  scale_shape_manual(name='Study',
-                     breaks=c(df_studies$Author,'Combined'),
-                     values=c(1:nlevels(as.factor(df_output_indiv$Study)),16),
-                     labels=c(df_studies$Label,'Combined Estimate'))+
-  scale_color_manual(name='Study',
-                     breaks=c(df_studies$Author,'Combined'),
-                     values=c(col_scale,'black'),
-                     labels=c(df_studies$Label,'Combined Estimate'))+
-  theme_classic() +
-  theme(axis.title.x=element_blank(),
-        axis.ticks.x=element_blank(),
-        axis.title = element_text(size=12),
-        axis.text = element_text(size=10),
-        legend.text = element_text(size=10))
-show(plot_RE)
-ggsave('Output/Figures/Relative Efficacy.png',height=5,width=4)
-
-plot_efficacy<-ggarrange(eff_plot,plot_RE,labels=c('A','B'),ncol=2,common.legend = TRUE,widths=c(5,3),legend='right')
-show(plot_efficacy)
-ggsave('Output/Figures/Manuscript_efficacy.jpg',
-       plot=plot_efficacy,
-       height=4,width = 6,dpi=1000)
-ggsave('Output/Figures/Manuscript_efficacy.eps',
-       plot=plot_efficacy,
-       height=4,width = 6,dpi=1000)
-
+#Save samples for individual effectiveness
 Efficacies_df=data.frame(Effs)
 colnames(Efficacies_df)<-levels(df_data$Indiv)[1:(nlevels(df_data$Indiv)-1)]
-write.csv(Efficacies_df,'Output/data/efficacies_vaccines.csv')
-
-
-
-# Fit universal efficacy model --------------------------------------------
-data_one <- list(
-  J = nrow(df_vacc),
-  Num_pop = df_data$Pop,
-  num_cases=df_data$Case,
-  num_groups=nlevels(df_data$group),
-  group_index=as.integer(df_data$group),
-  num_vaccines=1,
-  Vaccine_ind=rep(1,nrow(df_vacc)),
-  num_vacc_studies=nlevels(df_data$index),
-  vacc_study_ind=as.integer(df_data$index)
-)
-
-eff_fit_one <- stan(
-  file = "Stan/Efficacy_regress.stan",  # Stan program
-  data = data_one,    # named list of data
-  chains = 4,             # number of Markov chains
-  warmup = 10000,          # number of warmup iterations per chain
-  iter = 40000,            # total number of iterations per chain
-  seed=10,
-  cores=4,
-  control = list(max_treedepth=12,stepsize=0.5,adapt_delta=0.9)
-)
-
-eff_fit_one_pooled <- stan(
-  file = "Stan/Efficacy_pooled.stan",  # Stan program
-  data = data_one,    # named list of data
-  chains = 4,             # number of Markov chains
-  warmup = 10000,          # number of warmup iterations per chain
-  iter = 40000,            # total number of iterations per chain
-  seed=10,
-  cores=4,
-  control = list(max_treedepth=12,stepsize=0.5,adapt_delta=0.9)
-)
-
-samples<-rstan::extract(eff_fit_one)
-Effs<-(samples$p_s)
-quants<-t(apply(Effs,2,quantile,probs=c(0.025,0.5,0.975)))
-df_output<-data.frame(quants)
-df_output$Study=levels(df_data$index)
-
-plot1<-ggplot(df_output,aes(x=Study,y=X50.))
-plot1<-plot1+geom_point(position=position_dodge(width=0.5))
-plot1<-plot1+geom_errorbar(aes(ymin=X2.5.,ymax=X97.5.), position=position_dodge(width=0.5))
-show(plot1)
-
-loo1<-waic(extract_log_lik(eff_fit))
-loo2<-waic(extract_log_lik(eff_fit_one))
-loo3<-waic(extract_log_lik(eff_fit_individual))
-loo4<-waic(extract_log_lik(eff_fit_pooled))
-loo5<-waic(extract_log_lik(eff_fit_one_pooled))
-print(loo_compare(loo1,loo2,loo3,loo4,loo5))
-
-elppd1<-elpd(extract_log_lik(eff_fit))
-elppd2<-elpd(extract_log_lik(eff_fit_one))
-elppd3<-elpd(extract_log_lik(eff_fit_individual))
-elppd4<-elpd(extract_log_lik(eff_fit_pooled))
-elppd5<-elpd(extract_log_lik(eff_fit_one_pooled))
-print(loo_compare(elppd1,elppd2,elppd3,elppd4,elppd5))
+write.csv(Efficacies_df,'Output/samples/individual-effectiveness.csv')
