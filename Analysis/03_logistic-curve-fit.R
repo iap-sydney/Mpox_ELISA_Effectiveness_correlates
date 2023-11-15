5b82ebb133065f1300db09c5c4f79c5c4d845536
mtb980 <51979570+mtb980@users.noreply.github.com>
2023-11-15 17:11:44 +1100

    remove secondary models from analysis

--- Analysis/03_logistic-curve-fit.R
+++ Analysis/03_logistic-curve-fit.R
@@ -103,64 +103,6 @@ for (i in 1:N){
 
 write_csv(data.frame(r),'Output/samples/correlation.csv')
 
-
-# Logistic Fit ------------------------------------------------------------
-
-# Define the model data
-data <- list(
-  N = nrow(datatab),
-  y_n = log10(datatab$GMT),
-  s_n= datatab$std,
-  n_n=datatab$Number,
-  num_studies=nlevels(datatab$Study),
-  num_form=nlevels(datatab$Formulation),
-  study_ind=as.integer(datatab$Study),
-  dose_ind=as.integer(datatab$Dose),
-  form_ind=as.integer(datatab$Formulation),
-  J = nrow(df_eff),
-  Num_pop = df_eff$Pop,
-  num_cases=df_eff$Case,
-  num_groups=nlevels(df_eff$group),
-  group_index=as.integer(df_eff$group),
-  num_vaccines=nlevels(df_eff$Vacc_dose)-1,
-  Vaccine_ind=as.integer(df_eff$Vacc_dose),
-  num_vacc_studies=nlevels(df_eff$index),
-  vacc_study_ind=as.integer(df_eff$index)
-)
-
-#Fit the logistic model
-log_fit <- stan(
-  file = "Stan/linear_model.stan",  # Stan program
-  data = data,    # named list of data
-  chains = 4,             # number of Markov chains
-  warmup = warmup,          # number of warmup iterations per chain
-  iter = sampling,            # total number of iterations per chain
-  seed=10,
-  cores=4,
-  control = list(max_treedepth=12,stepsize=0.5,adapt_delta=0.9)
-)
-
-linear_samples <- rstan::extract(log_fit)
-slope<-linear_samples$k
-Intercept<-linear_samples$A
-N <- 61
-lin_fit_df <- data.frame(slope,Intercept)
-write.csv(lin_fit_df,'Output/samples/logistic_curve_parameters_GMT.csv')
-
-#Estimate Effectiveness for varying means with fixed standard deviation
-eff_lin_df <- data.frame(row.names = c('LCI','median','UCI'))
-E <- rep(0,1000)
-GMTs <- seq(1,4,length.out=N)
-for (i in 1:N){
-  for (j in 1:length(E)){
-    E[j] <- mean(1/(1+exp(-slope[j]*(0.5*sample+GMTs[i]-2.5)-Intercept[j])))
-  }
-  eff_lin_df[,i] <- quantile(E,probs = c(0.025,0.5,0.975))
-}
-eff_lin_df <- as.data.frame(t(eff_lin_df))
-eff_lin_df$GMT <- GMTs
-write.csv(eff_lin_df,'Output/Figures/Efficacy_quantiles_lin.csv')
-
 # Parameter estimates -----------------------------------------------------
 
 samples <- rstan::extract(full_fit)
@@ -173,16 +115,5 @@ sigma_I<-samples$sigma_s
 sigma_E<-samples$sigma
 
 
-# Flat fit ----------------------------------------------------------------
 
-flat_fit <- stan(
-  file = "Stan/flat_model.stan",  # Stan program
-  data = data,    # named list of data
-  chains = 4,             # number of Markov chains
-  warmup = warmup,          # number of warmup iterations per chain
-  iter = sampling,            # total number of iterations per chain
-  seed=10,
-  cores=4,
-  control = list(max_treedepth=12,stepsize=0.5,adapt_delta=0.9)
-)
 
--- Analysis/07_Supplementary-Figures.R
+++ Analysis/07_Supplementary-Figures.R
@@ -75,7 +75,7 @@ ggsave('Output//Figures//supp_4.pdf',
 q_df<-read_csv('Output/Figure4/Decay_quantiles.csv')%>%
   mutate(Group=factor(Group),Time=Time*12)%>%
   mutate_at(c('GMT','UCI','LCI'),round)%>%
-  mutate(CI=paste('(',LCI,'.',UCI,')',sep=''))%>%
+  mutate(CI=paste('(',LCI,'-',UCI,')',sep=''))%>%
   mutate(vals=paste(GMT,CI))%>%
   select(Time,vals,Group)%>%
   filter(Time %in% c(1,2,3,6,12,18,24))%>%
@@ -85,72 +85,3 @@ q_df<-read_csv('Output/Figure4/Decay_quantiles.csv')%>%
 
 c(1,5,9,7)
 c('1-Dose','2-Dose (Day-28)','2-Dose (Day-730)','3-Dose')
-
-
-# Generate linear model fit -----------------------------------------------
-
-# Load Data ---------------------------------------------------------------
-
-#import sampled means
-mean_df<-read_csv('Output/Samples/antibody_means.csv')
-
-#import sampled efficacies
-Efficacies_df<-read_csv('Output/Samples/Combined-Effectiveness-sample.csv')
-
-
-#Import Effectiveness quantiles with GMT
-eff_df<-read_csv('Output/Figures/Efficacy_quantiles_lin.csv')
-eff_df$LCI<-log(1-eff_df$LCI)
-eff_df$median<-log(1-eff_df$median)
-eff_df$UCI<-log(1-eff_df$UCI)
-
-
-# pair samples from effectiveness and immunogencity -----------------------
-
-#MVA-BN 1 dose set
-dose1df<-data.frame(mean_df$`MVA-BN 1-dose`,log(1-Efficacies_df$`MVA-BN 1-dose`/100))
-colnames(dose1df)<-c('GMT','Efficacy')
-dose1df$Vaccine='MVA-BN 1-dose'
-
-#MVA-BN 2 dose set
-dose2df<-data.frame(mean_df$`MVA-BN 2-dose`,log(1-Efficacies_df$`MVA-BN 2-dose`/100))
-colnames(dose2df)<-c('GMT','Efficacy')
-dose2df$Vaccine='MVA-BN 2-dose'
-
-#Historic Vaccination set
-doseddf<-data.frame(mean_df$`Historic Vaccination`,log(1-Efficacies_df$`First Generation`/100))
-colnames(doseddf)<-c('GMT','Efficacy')
-doseddf$Vaccine='First Generation'
-
-#combine sampled data
-big_df=rbind(dose1df,dose2df,doseddf)
-
-
-# Figure 3 ----------------------------------------------------------------
-
-#generate figure
-lin_plot<-ggplot(eff_df,aes(x=GMT,y=median)) +
-  geom_line(color='black')+
-  geom_ribbon(aes(ymin=LCI,ymax=UCI),color='black',fill='black',alpha=0.3,linetype=2)+
-  geom_density_2d(data=big_df,aes(y=Efficacy,color=Vaccine),
-                  contour_var = 'ndensity',
-                  bins=5,size=0.5)+
-  scale_color_manual(breaks = levels(as.factor(big_df$Vaccine)),
-                     values= colV,
-                     label=levels(as.factor(big_df$Vaccine)))+
-  theme_classic()+
-  scale_y_continuous('log(OR)') +
-  scale_x_continuous(breaks=1:4,labels=10^(1:4))+
-  theme(legend.text = element_text(size=10),
-        legend.title = element_text(size=12),
-        axis.title = element_text(size=12),
-        axis.text = element_text(size=10)
-  )
-
-#Save Figure
-ggsave("Output//Figures//lin_curve_fit.jpg",
-       plot=logistic_plot,
-       width=5,height=4,dpi=1000)
-ggsave("Output//Figures//lin_curve_fit.pdf",
-       plot=logistic_plot,
-       width=5,height=4,dpi=1000)
